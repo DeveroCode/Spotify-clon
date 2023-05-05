6fd4032e4f8cc3d3459864cfa7ec16374952556d
CMartinez-BB <charli.rey.misterio@gmail.com>
2023-05-05 13:17:51 -0500

    Update: finished functions and added "Recientemente" JSON file

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,9 +1,15 @@
-import data from '../json/echo-para..json'assert { type: "json" };
+import data from '../json/echo-para..json' assert { type: "json" };
+import mixesP from '../json/mixes.json' assert { type: "json" };
+import recientemente from '../json/reciente.json' assert { type: "json" };
+
 const container = document.querySelector('.playlist');
+const containerMixes = document.querySelector('.mixes');
 const screenWidth = window.innerWidth;
 
 window.onload = function () {
     getAllPlaylist();
+    getMixes();
+    getRecientemente();
 }
 
 
@@ -52,3 +58,89 @@ function getAllPlaylist() {
     window.addEventListener('resize', getAllPlaylist);
 }
 
+function getMixes() {
+    const mixes = Object.values(mixesP.mixes);
+    containerMixes.innerHTML = '';
+    mixes.forEach((mix, index) => {
+        if (screenWidth >= 988) {
+            const card = document.createRange().createContextualFragment(`
+                <div class="playlist__cont">
+                    <div class="playlist-image">
+                        <img src=${mix.album_image} alt="${mix.playlist}">
+                    </div>
+                    <div class="playlist__information">
+                        <h3>${mix.playlist}</h3>
+                        <span class="uppercase">
+                            ${mix.description}
+                        </span>
+                    </div>
+                </div>
+            `);
+
+            containerMixes.append(card);
+        } else if (screenWidth < 999 && index < 3) {
+            const card = document.createRange().createContextualFragment(`
+                <div class="playlist__cont">
+                    <div class="playlist-image">
+                        <img src=${mix.album_image} alt="${mix.playlist}">
+                    </div>
+                    <div class="playlist__information">
+                        <h3>${mix.playlist}</h3>
+                        <span class="uppercase">
+                            ${mix.description}
+                        </span>
+                    </div>
+                </div>
+            `);
+
+            containerMixes.append(card);
+        }
+    });
+
+    window.addEventListener('resize', getMixes);
+}
+
+
+function getRecientemente() {
+    const recientes = Object.values(recientemente.reciente);
+    const containerRecientes = document.querySelector('.reciente');
+    containerRecientes.innerHTML = '';
+
+    recientes.forEach((reciente, index) => {
+        if (screenWidth >= 988) {
+            const card = document.createRange().createContextualFragment(`
+                <div class="playlist__cont">
+                    <div class="playlist-image">
+                        <img src=${reciente.album_image} alt="${reciente.playlist}">
+                    </div>
+                    <div class="playlist__information">
+                        <h3>${reciente.playlist}</h3>
+                        <span class="uppercase">
+                            ${reciente.description}
+                        </span>
+                    </div>
+                </div>
+            `);
+
+            containerRecientes.append(card);
+        } else if (screenWidth < 999 && index < 3) {
+            const card = document.createRange().createContextualFragment(`
+                <div class="playlist__cont">
+                    <div class="playlist-image">
+                        <img src=${reciente.album_image} alt="${reciente.playlist}">
+                    </div>
+                    <div class="playlist__information">
+                        <h3>${reciente.playlist}</h3>
+                        <span class="uppercase">
+                            ${reciente.description}
+                        </span>
+                    </div>
+                </div>
+            `);
+
+            containerRecientes.append(card);
+        }
+    });
+
+    window.addEventListener('resize', getRecientemente);
+}   
